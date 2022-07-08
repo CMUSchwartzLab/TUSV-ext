19a3037460d1b30f3e0b3f25147b8b61d6e58552
Cindy Fu <44757116+cindyfu@users.noreply.github.com>
2022-07-08 16:08:57 -0400

    Update snv_matching
    Update snv_matching with a more suitable mechanism for paired breakpoints

--- model/snv_matching.py
+++ model/snv_matching.py
@@ -22,93 +22,86 @@ def one_hot_encoding(index_list, category_list):
     encoding[:, index_list] = 1
     return encoding
 
-def snv_assign(C_CNV, Q, A, E, U, F):
+def snv_assign(C_CNV, Q, A, E, U, F, G):
     """
     the function for assigning unsampled SNVs to the trees, using brutal force with minimum
     distance criteria to identify the possible branch and allele of a SNV given
     n - number of clones
     m - number of samples
     l - number of SVs
+    l_un - number of unsampled SVs
     g - number of sampled SNVs
     g_un - number of unsampled SNVs
     r - number of CNVs
     :param C_CNV: n*2r allelic specific CNV
-    :param Q: g_un * r mapping matrix which maps the unsampled SNVs to CNV segments, q_ij=1 if ith SNV maps to jth CNV
+    :param Q: (l_un + g_un) * r mapping matrix which maps the unsampled SNVs to CNV segments, q_ij=1 if ith SNV maps to jth CNV
     :param A: n*n, a_ij = 1 if i is the ancestor of j, diagonal is 0, which means i is not the ancestor of i
     :param U: m*n frequency matrix
     :param F: m*g_un frequency matrix
+    :param G: l_un*l_un unsampled breakpoints pairing matrix
     :return:
     """
     n, r = C_CNV.shape
-    g_un = Q.shape[0]
+    l_g_un = Q.shape[0]
+    l_un = G.shape[0]
     r = int(r/2)
     clone_idx_range = range(0, n-1) # exclude the root node
-    C_hat_1 = np.dot(C_CNV[:, :r], np.transpose(Q)) # n*g_un, the copy number of CNV at SNV position
-    C_hat_2 = np.dot(C_CNV[:, r:], np.transpose(Q))  # n*g_un, the copy number of CNV at SNV position
+    C_hat_1 = np.dot(C_CNV[:, :r], np.transpose(Q)) # n*l_g_un, the copy number of CNV at SNV position
+    C_hat_2 = np.dot(C_CNV[:, r:], np.transpose(Q))  # n*l_g_un, the copy number of CNV at SNV position
     C_hat_1_parent = np.dot(E.T, C_hat_1)
     C_hat_2_parent = np.dot(E.T, C_hat_2)
-    min_dist = np.full((g_un), np.inf)
-    min_node = np.full((g_un), -1)
+    min_dist = np.full((l_g_un), 10000)
+    min_node = np.full((l_g_un), -1)
+    dist = np.full((l_g_un), np.inf)
     for b in clone_idx_range:
-        #print(b)
         ### normal copy number=1
-        C_SNV_clone_1 = C_hat_1[b, :] # g_un
+        C_SNV_clone_1 = C_hat_1[b, :] # l_g_un
         C_SNV_clone_2 = C_hat_2[b, :]
         C_SNV_clone_parent_1 = C_hat_1_parent[b, :]
         C_SNV_clone_parent_2 = C_hat_2_parent[b, :]
+
         valid_snv_idx = np.array(list(set(np.append(np.where(C_SNV_clone_1 == 1)[0],np.where(C_SNV_clone_1 - C_SNV_clone_parent_1 > 1)[0]))))
-        #print(U[:,b][:,np.newaxis] ,C_SNV_clone_1[valid_snv_idx],U[:,b][:,np.newaxis] * C_SNV_clone_1[valid_snv_idx])
         F_est = U[:,b][:,np.newaxis] * C_SNV_clone_1[valid_snv_idx] + np.dot(U, A[b, :][:,np.newaxis]* C_hat_1[:,valid_snv_idx])
-        #print('1',F_est, F[:, valid_snv_idx])
-        dist = np.sum(np.abs(F_est - F[:, valid_snv_idx]),axis=0)
-        dist_stack = np.column_stack((min_dist[valid_snv_idx], dist))
+        dist[valid_snv_idx] = np.sum(np.abs(F_est - F[:, valid_snv_idx]),axis=0)
+        #dist_stack = np.column_stack((min_dist[valid_snv_idx], dist))
+        # argmin = np.argmin(dist_stack, axis=-1)
+        # if (argmin == 1).any():
+        #     #print(min_node[valid_snv_idx[argmin == 1]])
+        #     min_node[valid_snv_idx[argmin == 1]] = b
+        #     #print(min_node[valid_snv_idx[argmin == 1]])
+        #     min_dist[valid_snv_idx] = np.min(dist_stack, axis=-1)
+        #     #print(min_node, min_dist)
+
+        ### copy number > 1
+        valid_snv_idx2 = np.where(C_SNV_clone_1 > 1)[0]
+        F_est = U[:, b][:, np.newaxis] + np.dot(U, A[b, :][:, np.newaxis] * C_hat_1[:, valid_snv_idx2] / C_SNV_clone_1[
+                                                    valid_snv_idx2])
+        dist[valid_snv_idx2] = np.sum(np.abs(F_est - F[:, valid_snv_idx2]), axis=0)
+
+        dist[: l_un] += np.dot(dist[:l_un], G) #add the other corresponding breakpoint distance to original breakpoint to ensure paired breakpoints are at the same node
+        dist[: l_un] /= 2
+        dist_stack = np.column_stack((min_dist, dist))
         argmin = np.argmin(dist_stack, axis=-1)
         if (argmin == 1).any():
-            #print(min_node[valid_snv_idx[argmin == 1]])
-            min_node[valid_snv_idx[argmin == 1]] = b
-            #print(min_node[valid_snv_idx[argmin == 1]])
-            min_dist[valid_snv_idx] = np.min(dist_stack, axis=-1)
-            #print(min_node, min_dist)
+            min_node[argmin == 1] = b
+            min_dist = np.min(dist_stack, axis=-1)
 
         valid_snv_idx = np.array(list(set(np.append(np.where(C_SNV_clone_2 == 1)[0],np.where(C_SNV_clone_2 - C_SNV_clone_parent_2 > 1)[0]))))
         F_est = U[:, b][:,np.newaxis] * C_SNV_clone_2[valid_snv_idx] + np.dot(U, A[b, :][:, np.newaxis] * C_hat_2[:, valid_snv_idx])
-        #print('2',F_est, F[:, valid_snv_idx])
-        dist = np.sum(np.abs(F_est - F[:, valid_snv_idx]),axis=0)
-        dist_stack = np.column_stack((min_dist[valid_snv_idx], dist))
-        argmin = np.argmin(dist_stack, axis=-1)
-        if (argmin == 1).any():
-            #print(min_node[valid_snv_idx[argmin == 1]])
-            min_node[valid_snv_idx[argmin == 1]] = b
-            #print(min_node[valid_snv_idx[argmin == 1]])
-            min_dist[valid_snv_idx] = np.min(dist_stack, axis=-1)
-            #print(min_node, min_dist)
+        dist[valid_snv_idx] = np.sum(np.abs(F_est - F[:, valid_snv_idx]),axis=0)
 
-        ### copy number > 1
-        valid_snv_idx = np.where(C_SNV_clone_1 > 1)[0]
-        F_est = U[:, b][:,np.newaxis] + np.dot(U, A[b, :][:, np.newaxis] * C_hat_1[:, valid_snv_idx] / C_SNV_clone_1[valid_snv_idx])
-        #print('3',F_est, F[:, valid_snv_idx])
-        dist = np.sum(np.abs(F_est - F[:, valid_snv_idx]),axis=0)
-        dist_stack = np.column_stack((min_dist[valid_snv_idx], dist))
-        argmin = np.argmin(dist_stack, axis=-1)
-        if (argmin == 1).any():
-            #print(min_node[valid_snv_idx[argmin == 1]])
-            min_node[valid_snv_idx[argmin == 1]] = b
-            #print(min_node[valid_snv_idx[argmin == 1]])
-            min_dist[valid_snv_idx] = np.min(dist_stack, axis=-1)
-            #print(min_node, min_dist)
+        valid_snv_idx2 = np.where(C_SNV_clone_2 > 1)[0]
+        F_est = U[:, b][:,np.newaxis] + np.dot(U, A[b, :][:, np.newaxis] * C_hat_2[:, valid_snv_idx2] / C_SNV_clone_2[valid_snv_idx2])
+        dist[valid_snv_idx2] = np.sum(np.abs(F_est - F[:, valid_snv_idx2]),axis=0)
 
-        valid_snv_idx = np.where(C_SNV_clone_2 > 1)[0]
-        F_est = U[:, b][:,np.newaxis] + np.dot(U, A[b, :][:, np.newaxis] * C_hat_2[:, valid_snv_idx] / C_SNV_clone_2[valid_snv_idx])
-        #print('4',F_est, F[:, valid_snv_idx])
-        dist = np.sum(np.abs(F_est - F[:, valid_snv_idx]),axis=0)
-        dist_stack = np.column_stack((min_dist[valid_snv_idx], dist))
+        dist[: l_un] += np.dot(dist[:l_un], G) #add the other corresponding breakpoint distance to original breakpoint to ensure paired breakpoints are at the same node
+        dist[: l_un] /= 2
+
+        dist_stack = np.column_stack((min_dist, dist))
         argmin = np.argmin(dist_stack, axis=-1)
         if (argmin == 1).any():
-            #print(min_node[valid_snv_idx[argmin == 1]])
             min_node[valid_snv_idx[argmin == 1]] = b
-            #print(min_node[valid_snv_idx[argmin == 1]])
             min_dist[valid_snv_idx] = np.min(dist_stack, axis=-1)
-            #print(min_node, min_dist)
     W_snv = np.zeros((n, len(min_node)))
     for i in range(len(min_node)):
         W_snv[min_node[i], i] = 1
@@ -122,10 +115,10 @@ if __name__ == '__main__':
     m=1
     g_un=5
     k=4
-    np.random.seed(0)
+    #np.random.seed(0)
     U = np.array([[0.1, 0.5, 0.3, 0.1],[0.2, 0.2, 0.6, 0]])
     C_CNV = np.array([[1,2,1,1,1,1,4,1],
-                      [1,2,1,3,2,2,1,1],
+                      [2,2,1,3,1,1,1,1],
                       [1,2,1,1,1,1,1,1],
                       [1,1,1,1,1,1,1,1],])
     A = np.array([[0, 0, 0, 0],
@@ -133,13 +126,15 @@ if __name__ == '__main__':
                   [1, 1, 0, 0],
                   [1, 1, 1, 0]])
     Q = np.eye(4)
+    G = np.eye(2)
     C_SNV = np.array([[1, 1, 4, 0],
-                      [0, 1, 1, 1],
-                      [0, 1, 1, 0],
+                      [2, 1, 1, 1],
+                      [1, 1, 1, 0],
                       [0, 0, 0, 0]])
     E = np.array([[0, 0, 0, 0],
                   [0, 0, 0, 0],
                   [1, 1, 0, 0],
                   [0, 0, 1, 0]])
-    F = np.dot(U, C_SNV)
-    min_node, min_dist, W_snv = snv_assign(C_CNV, Q, A, E, U, F)
+    F_true = np.dot(U, C_SNV)
+    F_noise = F_true + np.random.normal(scale=0.2,size=np.shape(F_true) )
+    min_node, min_dist, W_snv = snv_assign(C_CNV, Q, A, E, U, F_noise, G)
